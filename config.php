0b923c8999a7a829b5621bf9584141272f97ecdd
Aws Ayyash <74304034+AwsAyyash@users.noreply.github.com>
2022-09-11 18:55:07 +0300

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DBHOST', 'web1190680.studentswebprojects.ritaj.ps');
-define('DBNAME', 'c433proj_flat_rental');
-define('DBUSER', 'c433proj1190680');
-define('DBPASS', 'pass1190680');
-define('DBCONNSTRING', 'mysql:dbname=c433proj_flat_rental;charset=utf8mb4;');
