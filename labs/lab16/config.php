c7e38c8e869df917fcc8e530090d4fa3176a8826
Aws Ayyash <74304034+AwsAyyash@users.noreply.github.com>
2023-03-20 14:47:48 +0200

    Delete labs directory

--- labs/lab16/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DBHOST', 'localhost');
-define('DBNAME', 'c433login');
-define('DBUSER', 'testuser');
-define('DBPASS', 'mypassword');
-define('DBCONNSTRING', 'mysql:dbname=c433login;charset=utf8mb4;');
